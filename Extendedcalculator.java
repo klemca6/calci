1e75ea6693d93db30bc8334d37fa4d1e39f6c877
klemca2 <mca2@kletech.ac.in>
2024-05-16 13:00:57 +0530

    extended calcu

---
+++ Extendedcalculator.java
@@ -0,0 +1,22 @@
+public class Extendedcalculator {
+    public void Square(int a)
+    {
+        int sqa=a*a;
+        System.out.print("\nSquare:"+sqa);
+    }
+    public void Modulas(int a,int b)
+    {
+        int modu=a%b;
+        System.out.print("\nModulas:"+modu);
+    }
+    public void Cuberoot(int a)
+    {
+        System.out.print("\nCuberoot:"+Math.cbrt(a));
+    }
+}
+
+ 
+
+ 
+
+          
