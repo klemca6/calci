e9ea9f95ff8ba3ab21350bc1cfb352a8caebba2c
klemca2 <mca2@kletech.ac.in>
2024-05-16 12:37:12 +0530

    code for calculator

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+public class Calculator {
+    public void Additon(int a,int b)
+    {
+        int sum=a+b;
+        System.out.print("Addition:"+sum);
+    }
+    public void Substraction (int a,int b)
+    {
+        int sub=a-b;
+        System.out.print("\nSubstraction:"+sub);
+    }
+    public void  Multiplication(int a,int b)
+    {
+        int mul=a*b;
+        System.out.print("\nMultiplication:"+mul);
+    }
+    public void   Division(int a,int b)
+    {
+        int div=a/b;
+        System.out.print("\nDivision:"+div);
+    }
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=5;
+        Calculator c = new Calculator();
+         c.Additon(a,b);
+         c.Substraction(a, b);
+         c.Multiplication(a, b);
+         c.Division(a, b);
+    }
+
+}
